c0ab6bf855c6a07845976ffb545f1023616fbaf3
Julio <juliogtrenard@gmail.com>
2024-09-26 14:04:11 +0200

    ADD Estructura básica visual terminada

--- src/main/java/es/juliogtrenard/calculadorafxml/HelloController.java
+++ src/main/java/es/juliogtrenard/calculadorafxml/HelloController.java
@@ -1,14 +1,41 @@
 package es.juliogtrenard.calculadorafxml;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField txtNum1;
+
+    @FXML
+    private TextField txtNum2;
+
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    private Button btnCalcular;
+
+    @FXML
+    private RadioButton rbSumar;
+
+    @FXML
+    private RadioButton rbRestar;
+
+    @FXML
+    private RadioButton rbMultiplicar;
+
+    @FXML
+    private RadioButton rbDividir;
+
+    @FXML
+    private ToggleGroup tgOperacion;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
